0f0e4aceb1f37fc215f708667bac0815f2416b10
Emmanuel Zabala <emmanuelzabalamagrini@upc.edu.ar>
2025-10-01 20:48:33 -0300

    feat(inventory-system): add player collision & AddItem method call

--- Assets/Scripts/Inventory/Pickup.cs
+++ Assets/Scripts/Inventory/Pickup.cs
@@ -4,4 +4,14 @@ public class Pickup : MonoBehaviour
 {
     [SerializeField] private ItemSO item;
     [SerializeField] private int quantity = 1;
+    
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        InventoryManager inventoryManager = other.GetComponent<InventoryManager>();
+        if (inventoryManager != null)
+        {
+            inventoryManager.AddItem(item, quantity);
+            Destroy(gameObject); 
+        }
+    }
 }
