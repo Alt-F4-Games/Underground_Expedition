fc04cd168760a63ecf230ad7488dbdec4be60812
Emmanuel Zabala <emmanuelzabalamagrini@upc.edu.ar>
2025-10-01 20:26:40 -0300

    feat(inventory-system): add AddItem method to InventoryManager

--- Assets/Scripts/Inventory/InventoryManager.cs
+++ Assets/Scripts/Inventory/InventoryManager.cs
@@ -6,5 +6,16 @@ public class InventoryManager : MonoBehaviour
     [SerializeField] private Inventory inventory = new Inventory();
     public UnityEvent OnInventoryChanged;
     
-    
+    public void AddItem(ItemSO item, int quantity = 1)
+    {
+        if (inventory.AddItem(item, quantity))
+        {
+            OnInventoryChanged.Invoke();
+            Debug.Log($"Added {item.itemName} x{quantity} to inventory");
+        }
+        else
+        {
+            Debug.Log("Inventory full or invalid item");
+        }
+    }
 }
