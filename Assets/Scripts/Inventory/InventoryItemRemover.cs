4e00956e6ee3a5bc9446c4bc37748140c6c2be94
Emmanuel Zabala <emmanuelzabalamagrini@upc.edu.ar>
2025-10-02 17:25:44 -0300

    feat(inventory-system): add InventoryItemRemover script to remove specific item on key press

---
+++ Assets/Scripts/Inventory/InventoryItemRemover.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public class InventoryItemRemover : MonoBehaviour
+{
+    [SerializeField] private InventoryManager inventoryManager;
+    [SerializeField] private ItemSO itemToRemove;
+}
