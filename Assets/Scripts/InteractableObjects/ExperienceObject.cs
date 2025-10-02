06c846191d46de607caf6f5af30df9c3a19f823a
Benjamin Bayarri <benjaminbayarri17@gmail.com>
2025-10-01 23:15:16 -0300

    feat(interaction): add HealthObject and ExperienceObject interactables
    - Implemented HealthObject script that restores player health when interacted
    - Implemented ExperienceObject script that grants XP through ExperienceSystem (WIP)

---
+++ Assets/Scripts/InteractableObjects/ExperienceObject.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class ExperienceObject : MonoBehaviour, IInteractable
+{
+    [SerializeField] private int xpAmount = 20; 
+
+    public void Interact(PlayerInteraction player)
+    {
+        Debug.Log($"Has recibido {xpAmount} de experiencia");
+        
+        if (ExperienceSystem.instance != null)
+        {
+            ExperienceSystem.instance.AddXP(xpAmount);
+        }
+        else
+        {
+            Debug.LogError("No se encontró un ExperienceSystem en la escena.");
+        }
+    }
+
+    public void Release()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/InteractableObjects/HealthObject.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class HealthObject : MonoBehaviour, IInteractable
+{
+    [SerializeField] private int healAmount = 20;
+
+    public void Interact(PlayerInteraction player)
+    {
+        Debug.Log("Has recibido curación");
+        
+        var health = player.GetComponent<HealthSystem>();
+        if (health != null)
+        {
+            health.Heal(healAmount);
+        }
+    }
+
+    public void Release()
+    {
+        
+    }
+}
