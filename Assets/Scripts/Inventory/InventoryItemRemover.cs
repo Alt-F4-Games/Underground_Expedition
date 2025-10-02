d9a601890f55ca89c3cc4c189d7ec713dbb1c5ad
Emmanuel Zabala <emmanuelzabalamagrini@upc.edu.ar>
2025-10-02 17:45:31 -0300

    chore(inventory-system): use Debug.LogError for unassigned fields in InventoryItemRemover

--- Assets/Scripts/Inventory/InventoryItemRemover.cs
+++ Assets/Scripts/Inventory/InventoryItemRemover.cs
@@ -4,4 +4,27 @@ public class InventoryItemRemover : MonoBehaviour
 {
     [SerializeField] private InventoryManager inventoryManager;
     [SerializeField] private ItemSO itemToRemove;
+    
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            RemoveSpecificItem();
+        }
+    }
+
+    private void RemoveSpecificItem()
+    {
+        if (inventoryManager == null)
+        {
+            Debug.LogError("InventoryManager not asigned to InventoryItemRemover.");
+            return;
+        }
+        
+        if (itemToRemove == null)
+        {
+            Debug.LogError("ItemToRemove not asigned to InventoryItemRemover.");
+            return;
+        }
+    }
 }
