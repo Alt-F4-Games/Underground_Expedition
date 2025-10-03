4c179a7847550883a1f08a44d7d76700c25698e4
Emmanuel Zabala <emmanuelzabalamagrini@upc.edu.ar>
2025-10-03 20:02:27 -0300

    feat(new-inventory-system): add ItemType enum and auto-config logic in ItemSO

--- Assets/Scripts/Inventory/ItemSO.cs
+++ Assets/Scripts/Inventory/ItemSO.cs
@@ -1,11 +1,49 @@
 using UnityEngine;
 
+public enum ItemType
+{
+    Pickup,
+    ToolActive,
+    ToolPassive,
+    KeyItem
+}
+
 [CreateAssetMenu(fileName = "NewItem", menuName = "Inventory/New Item")]
 public class ItemSO :  ScriptableObject
 {
+    [Header("General Info")]
     public string itemName;
     public Sprite icon;
-    public int maxStack = 10;
-    public bool isActiveTool;
-    public bool isTool;
+    public ItemType itemType;
+    public int maxStack = 1;
+    
+    [Header("Tool Properties")]
+    public bool hasDurability;
+    public int maxDurability;
+
+    [TextArea]
+    public string description;
+
+    private void OnValidate()
+    {
+        switch (itemType)
+        {
+            case ItemType.Pickup:
+                maxStack = 16;
+                hasDurability = false;
+                break;
+
+            case ItemType.ToolActive:
+            case ItemType.ToolPassive:
+                maxStack = 1;
+                hasDurability = true;
+                break;
+
+            case ItemType.KeyItem:
+                maxStack = 1;
+                hasDurability = false;
+                break;
+        }
+    }
+
 }
