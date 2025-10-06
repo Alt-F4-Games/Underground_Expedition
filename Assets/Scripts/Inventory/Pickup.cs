4b3037bc8e8f7c0bb80947ebe95ba18c8e63fe09
Emmanuel Zabala <emmanuelzabalamagrini@upc.edu.ar>
2025-10-06 18:46:27 -0300

    feat(pickup): add serialized ItemSO and quantity fields

--- Assets/Scripts/Inventory/Pickup.cs
+++ Assets/Scripts/Inventory/Pickup.cs
@@ -3,5 +3,8 @@ using UnityEngine;
 [RequireComponent(typeof(Collider))]
 public class Pickup : MonoBehaviour
 {
-   
+    [Header("Item Data")]
+    [SerializeField] private ItemSO item;
+    [SerializeField, Min(1)] private int quantity = 1;
+    
 }
