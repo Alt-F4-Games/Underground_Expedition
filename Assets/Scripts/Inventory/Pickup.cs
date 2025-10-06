7e7fd0fa489929793dd8cf30570542d38a31266b
Emmanuel Zabala <emmanuelzabalamagrini@upc.edu.ar>
2025-10-06 18:48:20 -0300

    feat(pickup): implement Reset to auto-enable collider trigger

--- Assets/Scripts/Inventory/Pickup.cs
+++ Assets/Scripts/Inventory/Pickup.cs
@@ -6,5 +6,11 @@ public class Pickup : MonoBehaviour
     [Header("Item Data")]
     [SerializeField] private ItemSO item;
     [SerializeField, Min(1)] private int quantity = 1;
-    
+
+    private void Reset()
+    {
+        var col = GetComponent<Collider>();
+        col.isTrigger = true;
+    }
+
 }
