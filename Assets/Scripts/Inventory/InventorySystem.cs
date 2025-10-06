89dbb8777a300bca80a5f195da74dcf4e116d136
Emmanuel Zabala <emmanuelzabalamagrini@upc.edu.ar>
2025-10-06 17:53:58 -0300

    feat(new-inventory-system): add SlotType enum and InventorySlot data class

--- Assets/Scripts/Inventory/InventorySystem.cs
+++ Assets/Scripts/Inventory/InventorySystem.cs
@@ -1,5 +1,22 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
+
+public enum SlotType { Base, Hotbar, Equip }
+
+[Serializable] 
+public class InventorySlot
+{
+    public ItemSO item;
+    public int quantity;
+
+    public InventorySlot(ItemSO item, int quantity)
+    {
+        this.item = item;
+        this.quantity = Mathf.Max(quantity, 1);
+    }
+}
+
 
 public class InventorySystem : MonoBehaviour
 {
