86502213d1e8dfb23db1c0a39b15a950872c4208
Emmanuel Zabala <emmanuelzabalamagrini@upc.edu.ar>
2025-10-06 18:28:50 -0300

    feat(inventory-manager): create InventoryManager MonoBehaviour stub

--- Assets/Scripts/Inventory/InventoryManager.cs
+++ Assets/Scripts/Inventory/InventoryManager.cs
@@ -1,38 +1,10 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
+[RequireComponent(typeof(InventorySystem))]
 public class InventoryManager : MonoBehaviour
 {
-    [SerializeField] private Inventory inventory = new Inventory();
-    public UnityEvent OnInventoryChanged;
     
-    public void AddItem(ItemSO item, int quantity = 1)
-    {
-        if (inventory.AddItem(item, quantity))
-        {
-            OnInventoryChanged.Invoke();
-            Debug.Log($"Added {item.itemName} x{quantity} to inventory");
-        }
-        else
-        {
-            Debug.Log("Inventory full or invalid item");
-        }
-    }
-    
-    public bool RemoveItem(ItemSO item, int quantity = 1)
-    {
-        if (inventory.RemoveItem(item, quantity))
-        {
-            OnInventoryChanged.Invoke();
-            Debug.Log($"Removed {quantity} x {item.itemName} from inventory.");
-            return true;
-        }
-        else
-        {
-            Debug.Log("Failed to remove item. Insufficient quantity.");
-            return false;
-        }
-    }
-    
-    public Inventory GetInventory() => inventory;
 }
