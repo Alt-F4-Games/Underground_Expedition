f03db249973d7635b4b82705f6b2966b247891dc
Emmanuel Zabala <emmanuelzabalamagrini@upc.edu.ar>
2025-10-06 18:45:35 -0300

    feat(pickup): create Pickup MonoBehaviour stub with RequireComponent

--- Assets/Scripts/Inventory/Pickup.cs
+++ Assets/Scripts/Inventory/Pickup.cs
@@ -1,17 +1,7 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Collider))]
 public class Pickup : MonoBehaviour
 {
-    [SerializeField] private ItemSO item;
-    [SerializeField] private int quantity = 1;
-    
-    private void OnTriggerEnter(Collider other)
-    {
-        InventoryManager inventoryManager = other.GetComponent<InventoryManager>();
-        if (inventoryManager != null)
-        {
-            inventoryManager.AddItem(item, quantity);
-            Destroy(gameObject); 
-        }
-    }
+   
 }
