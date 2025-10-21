1059eef61961328091f7cd0f8f432d902b809a4c
Emmanuel Zabala <emmanuelzabalamagrini@upc.edu.ar>
2025-10-21 18:48:56 -0300

    feat(network-player): add basic networked movement logic

--- Assets/Scripts/Network/NetworkPlayerController.cs
+++ Assets/Scripts/Network/NetworkPlayerController.cs
@@ -8,6 +8,8 @@ public class NetworkPlayerController : NetworkBehaviour
     private NetworkCharacterController _characterController;
     [SerializeField] private Renderer _renderer;
 
+    [SerializeField] private float _moveSpeed = 5f;
+    
     public override void Spawned()
     {
         if (HasInputAuthority)
@@ -24,5 +26,8 @@ public class NetworkPlayerController : NetworkBehaviour
     public override void FixedUpdateNetwork()
     {
         if (!GetInput(out NetworkInputPlayer inputPlayer)) return;
+        
+        Vector3 moveVector = inputPlayer.MoveDirection.normalized;
+        _characterController.Move(moveVector * _moveSpeed * Runner.DeltaTime);
     }
 }
