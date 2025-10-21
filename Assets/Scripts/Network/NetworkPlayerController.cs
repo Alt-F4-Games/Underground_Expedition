aef0b5e99bf05934038c3d02d7750b71450b20d4
Emmanuel Zabala <emmanuelzabalamagrini@upc.edu.ar>
2025-10-21 18:38:49 -0300

    feat(network-player): Add Renderer and highlight local player via InputAuthority

---
+++ Assets/Scripts/Network/NetworkPlayerController.cs
@@ -0,0 +1,17 @@
+﻿using Fusion;
+using Network;
+using UnityEngine;
+
+public class NetworkPlayerController : NetworkBehaviour
+{
+    [SerializeField] private Renderer _renderer;
+
+    public override void Spawned()
+    {
+        if (HasInputAuthority)
+        {
+            _renderer.material.color = Color.yellow;
+        }
+    }
+    
+}
