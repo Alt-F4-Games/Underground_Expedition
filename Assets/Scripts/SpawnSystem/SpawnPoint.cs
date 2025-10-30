987571be38444e09cd03e9318ecd205d00df6134
Benjamin Bayarri <benjaminbayarri17@gmail.com>
2025-10-30 20:00:38 -0300

    feat(spawn-system): add core scripts for spawn management
    Implemented the core components of the Spawn System:
    -Spawner: Handles object instantiation based on spawn IDs and lifetime.
    -SpawnPoint: Defines unique spawn positions with visual gizmos in the editor.
    -SpawnDatabase: Stores and retrieves spawnable objects by ID.

---
+++ Assets/Scripts/SpawnSystem/SpawnPoint.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class SpawnPoint : MonoBehaviour
+{
+    [Header("Identificador único del punto de spawn")]
+    public string spawnID;
+
+#if UNITY_EDITOR
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, 0.3f);
+        UnityEditor.Handles.Label(transform.position + Vector3.up * 0.3f, spawnID);
+    }
+#endif
+}
