ef4abde9dd5ffcc2a1c2636d24bb75bfd68e3e0f
Benjamin Bayarri <benjaminbayarri17@gmail.com>
2025-11-10 18:36:32 -0300

    feat(enemy): update Skull AI scripts and add Skull prefab
    Modified the Skull-specific scripts (SkullIA, SkullChaseState, SkullPatrolState) to improve behavior and state handling.
    Added the new Skull enemy prefab.
    Also updated the Rat prefab to ensure consistency with recent AI and prefab changes.

---
+++ Assets/Scripts/Enemy/States/SkullPatrolState.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using Enemy.States.Base;
+
+namespace Enemy.States.Skull
+{
+    public class SkullPatrolState : EnemyPatrolState
+    {
+        private SkullIA skull;
+
+        public SkullPatrolState(SkullIA enemy) : base(enemy)
+        {
+            skull = enemy;
+        }
+
+        public override void UpdateLogic()
+        {
+            base.UpdateLogic();
+
+            if (skull.CanSeePlayer())
+                stateMachine.ChangeState(new SkullChaseState(skull));
+        }
+    }
+}
