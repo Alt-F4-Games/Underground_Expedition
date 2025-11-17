147bc505d694fbe26b5eb3f8cbcbf7651b767484
Emmanuel Zabala <emmanuelzabalamagrini@upc.edu.ar>
2025-11-17 13:03:36 -0300

    refactor(inventory): prepare InventoryManager structure for expansion

--- Assets/Scripts/Inventory/Networking/NetworkInventoryManager.cs
+++ Assets/Scripts/Inventory/Networking/NetworkInventoryManager.cs
@@ -7,5 +7,25 @@ using UnityEngine;
 
 public class NetworkInventoryManager : MonoBehaviour
 {
-    
+    [Header("Future References")]
+    [SerializeField] private MonoBehaviour futureInventorySystem; // to be replaced
+
+    [Header("Events")]
+    public System.Action OnInventoryChanged;
+
+    // --- Public API to be implemented later ---
+    public void RequestAddPlaceholder()
+    {
+        Debug.Log("[InventoryManager] Add request placeholder");
+    }
+
+    public void RequestRemovePlaceholder()
+    {
+        Debug.Log("[InventoryManager] Remove request placeholder");
+    }
+
+    public void RequestMovePlaceholder()
+    {
+        Debug.Log("[InventoryManager] Move request placeholder");
+    }
 }
