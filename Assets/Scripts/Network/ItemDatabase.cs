3e1a76416bf2ec7ac30a613fb6b6cc9817876f6b
Emmanuel Zabala <emmanuelzabalamagrini@upc.edu.ar>
2025-11-17 12:10:07 -0300

    feat(ItemData): add initial ItemDatabase structure

---
+++ Assets/Scripts/Network/ItemDatabase.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Inventory/ItemDatabase")]
+public class ItemDatabase : ScriptableObject
+{
+    [System.Serializable]
+    public struct Entry
+    {
+        public string keyName; 
+        public ItemSO item;
+        public GameObject equipPrefab;
+    }
+
+    [SerializeField] private List<Entry> entries = new();
+}
