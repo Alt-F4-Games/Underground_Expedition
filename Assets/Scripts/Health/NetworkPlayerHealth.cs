7acb6985e9aad6138ff322c4bd6233943c3772c6
Emmanuel Zabala <emmanuelzabalamagrini@upc.edu.ar>
2026-03-18 11:36:32 -0300

    refactor(health): delete respawn logic for testing

--- Assets/Scripts/Health/NetworkPlayerHealth.cs
+++ Assets/Scripts/Health/NetworkPlayerHealth.cs
@@ -6,15 +6,6 @@ namespace Health
     [RequireComponent(typeof(NetworkObject))]
     public class NetworkPlayerHealth : NetworkHealthSystem
     {
-        [SerializeField] private float _respawnDelay = 5f;
-
-        private NetworkPlayerController _controller;
-
-        private void Awake()
-        {
-            _controller = GetComponent<NetworkPlayerController>();
-        }
-
         protected override void Death()
         {
             base.Death();
@@ -22,30 +13,8 @@ namespace Health
             if (!HasStateAuthority) return;
 
             Debug.Log($"{gameObject.name} died");
-
-            if (_controller != null)
-                _controller.enabled = false;
-
-            Runner.StartCoroutine(RespawnRoutine());
+            
         }
-
-        private System.Collections.IEnumerator RespawnRoutine()
-        {
-            yield return new WaitForSeconds(_respawnDelay);
-
-            Respawn();
-        }
-
-        private void Respawn()
-        {
-            transform.position = Vector3.zero;
-
-            Revive();
-
-            if (_controller != null)
-                _controller.enabled = true;
-
-            Debug.Log($"{gameObject.name} respawned");
-        }
+        
     }
 }
