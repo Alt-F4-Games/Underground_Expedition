cc9e6600c26ed474bdb6e8f18ac44eed5bd9eb91
Emmanuel Zabala <emmanuelzabalamagrini@upc.edu.ar>
2026-03-25 16:05:37 -0300

    feat(spawn-system): change spawning logic
    changed for more precision in the election of NetworkObjects in spawnPoints

--- Assets/Scripts/Network/Spawn/SpawnPoints.cs
+++ Assets/Scripts/Network/Spawn/SpawnPoints.cs
@@ -1,3 +1,4 @@
+using Fusion;
 using UnityEngine;
 
 public enum SpawnType
@@ -7,9 +8,52 @@ public enum SpawnType
     Destructible
 }
 
-public class SpawnPoints : MonoBehaviour
+public class SpawnPoints : NetworkBehaviour
 {
     public SpawnType spawnType;
+
+    [Header("Enemy / Destructible")]
+    public NetworkPrefabRef prefab;
+
+    [Header("Pickup")]
+    public int pickupId;
+    public int amount;
+
+    public void Spawn()
+    {
+        switch (spawnType)
+        {
+            case SpawnType.Enemy:
+            case SpawnType.Destructible:
+                SpawnNetworkObject();
+                break;
+
+            case SpawnType.Pickup:
+                SpawnPickup();
+                break;
+        }
+    }
+
+    private void SpawnNetworkObject()
+    {
+        if (prefab.IsValid == false)
+        {
+            Debug.LogWarning($"No prefab assigned in {name}");
+            return;
+        }
+
+        if (!HasStateAuthority) ;
+
+        Runner.Spawn(prefab, transform.position, transform.rotation);
+    }
+
+    private void SpawnPickup()
+    {
+        if (!HasStateAuthority) ;
+        var obj = Runner.Spawn(prefab, transform.position, transform.rotation);
+        if (obj.TryGetComponent(out NetworkWorldItem item))
+            item.Init(pickupId, amount); 
+    }
     
     public bool drawGizmos = true;
 
