893ab77b5a14073cc4a2269305cf42e0daa33e1d
Emmanuel Zabala <emmanuelzabalamagrini@upc.edu.ar>
2026-03-31 15:51:41 -0300

    feat(player-auth): add anonymous sign in

---
+++ Assets/Scripts/UGS/AuthInitializer.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using UnityEngine;
+using Unity.Services.Core;
+using Unity.Services.Authentication;
+using System.Threading.Tasks;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace UGS
+{
+    public class AuthInitializer : MonoBehaviour
+    {
+        [SerializeField] private Button signInButton;
+        
+        async void Start()
+        {
+            signInButton.interactable = false;
+            await UnityServices.InitializeAsync();
+            signInButton.interactable = true;
+            signInButton.onClick.AddListener(SignIn);
+        }
+        private async void SignIn()
+        {
+            signInButton.interactable = false;
+            try
+            {
+                await AuthenticationService.Instance.SignInAnonymouslyAsync();
+                
+            }
+            catch (Exception e)
+            {
+                Debug.LogError(e);
+                throw;
+            }
+            
+            Debug.Log("Player ID: " + AuthenticationService.Instance.PlayerId);
+            await Task.Delay(4000);
+            Debug.Log("Sign In Complete");
+            LoadNextScene();
+        }
+
+        private void LoadNextScene()
+        {
+            int currentIndex = SceneManager.GetActiveScene().buildIndex;
+            SceneManager.LoadScene(currentIndex + 1);
+        }
+    }
+}
