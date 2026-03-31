68d63797b4c8f5261c10a8a7af4344d6fffb032c
Emmanuel Zabala <emmanuelzabalamagrini@upc.edu.ar>
2026-03-31 17:24:31 -0300

    feat(player-auth): add Username&Password auth logic

--- Assets/Scripts/UGS/AuthInitializer.cs
+++ Assets/Scripts/UGS/AuthInitializer.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 using Unity.Services.Core;
 using Unity.Services.Authentication;
 using System.Threading.Tasks;
+using TMPro;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
@@ -10,18 +11,42 @@ namespace UGS
 {
     public class AuthInitializer : MonoBehaviour
     {
-        [SerializeField] private Button signInButton;
+        [SerializeField] private Button signInAnonymousButton;
+        [SerializeField] private Button signUpWithUsernamePassword;
+        [SerializeField] private Button signInWithUsernamePassword;
+        [SerializeField] private TMP_Text HelpText;
+
+        [Header("Username & Password")]
+        [SerializeField] private TMP_InputField usernameInputField;
+        [SerializeField] private TMP_InputField passwordInputField;
         
         async void Start()
         {
-            signInButton.interactable = false;
+            signInAnonymousButton.interactable = false;
+            signUpWithUsernamePassword.interactable = false;
+            signInWithUsernamePassword.interactable = false;
             await UnityServices.InitializeAsync();
-            signInButton.interactable = true;
-            signInButton.onClick.AddListener(SignIn);
+            signInAnonymousButton.interactable = true;
+            signUpWithUsernamePassword.interactable = true;
+            signInWithUsernamePassword.interactable = true;
+            
+            
+            signInAnonymousButton.onClick.AddListener(SignIn);
+            
+            signUpWithUsernamePassword.onClick.AddListener(() =>
+            {
+                SignUpWithUsernamePasswordAsync(usernameInputField.text, passwordInputField.text);
+            });
+            
+            
+            signInWithUsernamePassword.onClick.AddListener(() =>
+            {
+                SignInWithUsernamePasswordAsync(usernameInputField.text, passwordInputField.text);
+            });
         }
         private async void SignIn()
         {
-            signInButton.interactable = false;
+            signInAnonymousButton.interactable = false;
             try
             {
                 await AuthenticationService.Instance.SignInAnonymouslyAsync();
@@ -39,6 +64,60 @@ namespace UGS
             LoadNextScene();
         }
 
+        private async void SignUpWithUsernamePasswordAsync(string username, string password)
+        {
+            try
+            {
+                await AuthenticationService.Instance.SignUpWithUsernamePasswordAsync(username, password);
+                Debug.Log("SignUp is successful.");
+                HelpText.text = "SignUp is successful.";
+                
+            }
+            catch (AuthenticationException ex)
+            {
+                // Compare error code to AuthenticationErrorCodes
+                // Notify the player with the proper error message
+                Debug.LogException(ex);
+                HelpText.text = ex.Message;
+            }
+            catch (RequestFailedException ex)
+            {
+                // Compare error code to CommonErrorCodes
+                // Notify the player with the proper error message
+                Debug.LogException(ex);
+                HelpText.text = ex.Message;
+            }
+        }
+        
+        
+        private async void SignInWithUsernamePasswordAsync(string username, string password)
+        {
+            try
+            {
+                await AuthenticationService.Instance.SignInWithUsernamePasswordAsync(username, password);
+                Debug.Log("SignIn is successful.");
+                Debug.Log("Player ID: " + AuthenticationService.Instance.PlayerId);
+                HelpText.text = "SignIn is successful.";
+                await Task.Delay(4000);
+                LoadNextScene();
+            }
+            catch (AuthenticationException ex)
+            {
+                // Compare error code to AuthenticationErrorCodes
+                // Notify the player with the proper error message
+                Debug.LogException(ex);
+                HelpText.text = ex.Message;
+            }
+            catch (RequestFailedException ex)
+            {
+                // Compare error code to CommonErrorCodes
+                // Notify the player with the proper error message
+                Debug.LogException(ex);
+                HelpText.text = ex.Message;
+            }
+        }
+
+        
         private void LoadNextScene()
         {
             int currentIndex = SceneManager.GetActiveScene().buildIndex;
