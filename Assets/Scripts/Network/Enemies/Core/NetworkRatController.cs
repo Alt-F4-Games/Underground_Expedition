7cddbd551e800bfda3a036fee27a7ea6e3303f3e
Alejandro Bustos <sam.alejandro.bustos@gmail.com>
2026-04-13 16:00:40 -0300

    feat(rat): added rat animations

--- Assets/Scripts/Network/Enemies/Core/NetworkRatController.cs
+++ Assets/Scripts/Network/Enemies/Core/NetworkRatController.cs
@@ -1,4 +1,6 @@
-﻿using Fusion;
+﻿using System;
+using Fusion;
+using Health;
 using Network.Enemies.States;
 using UnityEngine;
 
@@ -26,6 +28,26 @@ namespace Network.Enemies.Variants
         [Tooltip("Physical size of the impact during the jump. Should be SMALLER than AttackRange.")]
         public float JumpHitboxRadius = 0.5f;
         
+        // Components
+        private Animator _animator;
+        private NetworkEnemyHealth _enemyHealth;
+
+        private void Awake()
+        {
+            _animator = GetComponent<Animator>();
+            _enemyHealth = GetComponent<NetworkEnemyHealth>();
+        }
+        
+        private void OnEnable()
+        {
+            _enemyHealth.OnDamageTaken += PlayHitAnimation;
+        }
+
+        private void OnDisable()
+        {
+            _enemyHealth.OnDamageTaken -= PlayHitAnimation;
+        }
+
         // STATE FACTORY METHODS (Overrides)
         
         public override INetworkState GetAttackState()
@@ -33,7 +55,49 @@ namespace Network.Enemies.Variants
             return new NetworkChargeState(JumpChargeTime, () => 
                 new NetworkJumpAttackState(JumpSpeed, JumpExtraDistance, AttackDamage, JumpHitboxRadius, JumpHeight));
         }
+        
+        protected override void HandleStateChanged()
+        {
+            if (_animator == null) return;
 
+            switch (CurrentState)
+            {
+                case NetworkEnemyState.Patrolling:
+                case NetworkEnemyState.Chasing:
+                    _animator.SetBool("IsMoving", true);
+                    _animator.SetBool("IsCharging", false);
+                    break;
+
+                case NetworkEnemyState.Charging:
+                    _animator.SetBool("IsMoving", false);
+                    _animator.SetBool("IsCharging", true);
+                    break;
+
+                case NetworkEnemyState.Jumping:
+                    _animator.SetBool("IsMoving", true);
+                    _animator.SetBool("IsCharging", false);
+                    break;
+
+                case NetworkEnemyState.Dead:
+                    _animator.SetBool("IsMoving", false);
+                    _animator.SetBool("IsCharging", false);
+                    _animator.SetTrigger("Die");
+                    break;
+
+                case NetworkEnemyState.Idle:
+                    _animator.SetBool("IsMoving", false);
+                    _animator.SetBool("IsCharging", false);
+                    break;
+            }
+        }
+
+        private void PlayHitAnimation()
+        {
+            if (_animator == null) return;
+            
+            _animator.SetTrigger("Hit");
+        }
+        
         // Draws debug spheres in the Unity Editor for AI ranges
         protected override void OnDrawGizmosSelected()
         {
