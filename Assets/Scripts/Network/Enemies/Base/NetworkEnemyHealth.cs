44ebeecf34483d881ddea4b31907f617632437fc
Emmanuel Zabala <emmanuelzabalamagrini@upc.edu.ar>
2026-04-29 19:28:10 -0300

    feat(enemyHealth): add TriggerEvent enemy take damage

--- Assets/Scripts/Network/Enemies/Base/NetworkEnemyHealth.cs
+++ Assets/Scripts/Network/Enemies/Base/NetworkEnemyHealth.cs
@@ -1,16 +1,25 @@
-﻿using Fusion;
+﻿using Events;
+using Fusion;
 using UnityEngine;
 
 namespace Health
 {
     public class NetworkEnemyHealth : NetworkDespawnOnDeath
     {
+        private EnemyDiedEvent _enemyDiedEventEvent = new ();
+
+        public override void TakeDamage(int damage, PlayerRef playerRef)
+        {
+            base.TakeDamage(damage, playerRef);
+            _enemyDiedEventEvent.exp = 10;
+            _enemyDiedEventEvent.killer = playerRef;
+            EventController.Instance.TriggerEvent(_enemyDiedEventEvent);
+        }
 
         protected override void Death()
         {
             // Execute base logic (IsAlive = false, etc.)
             base.Death();
-
             Debug.Log($"[ENEMY] {gameObject.name} has died.");
         }
     }
