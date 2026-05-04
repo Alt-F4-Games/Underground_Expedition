fbff9d8805bdfbdf3357ec5c80df4c9ecf70f5d4
Emmanuel Zabala <emmanuelzabalamagrini@upc.edu.ar>
2026-05-04 18:20:51 -0300

    feat(experience-system): change experience reward to Death()

--- Assets/Scripts/Network/Enemies/Base/NetworkEnemyHealth.cs
+++ Assets/Scripts/Network/Enemies/Base/NetworkEnemyHealth.cs
@@ -1,25 +1,28 @@
 ﻿using Events;
 using Fusion;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace Health
 {
     public class NetworkEnemyHealth : NetworkDespawnOnDeath
     {
+        [SerializeField] private int _expPerKill;
+        
         private EnemyDiedEvent _enemyDiedEventEvent = new ();
 
         public override void TakeDamage(int damage, PlayerRef playerRef)
         {
             base.TakeDamage(damage, playerRef);
-            _enemyDiedEventEvent.exp = 10;
             _enemyDiedEventEvent.killer = playerRef;
-            EventController.Instance.TriggerEvent(_enemyDiedEventEvent);
         }
 
         protected override void Death()
         {
             // Execute base logic (IsAlive = false, etc.)
             base.Death();
+            _enemyDiedEventEvent.exp = _expPerKill;
+            EventController.Instance.TriggerEvent(_enemyDiedEventEvent);
             Debug.Log($"[ENEMY] {gameObject.name} has died.");
         }
     }
