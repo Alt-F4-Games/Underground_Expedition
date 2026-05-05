07051711635b427fd567f0f971a6c67749a1a058
Emmanuel Zabala <emmanuelzabalamagrini@upc.edu.ar>
2026-05-05 20:06:39 -0300

    feat(progression): add plus stats logic event

---
+++ Assets/Scripts/Network/Player/PlayerStatsManager.cs
@@ -0,0 +1,29 @@
+﻿using Events;
+using Fusion;
+using Local.Progression;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace Network
+{
+    public class PlayerStatsManager : NetworkBehaviour
+    {
+        [SerializeField] private int _plusMaxHealth = 10;
+        [SerializeField] private float _plusMaxStamimna = 5;
+        [SerializeField] private int _plusPlayerDamage;
+        
+        private PlayerStatsEvent  _playerStatsEvent = new ();
+        
+        public void ApplyStatsServer()
+        {
+            if (!HasStateAuthority) return;
+
+            _playerStatsEvent.MaxHealth =  _plusMaxHealth;
+            _playerStatsEvent.MaxStamina =  _plusMaxStamimna;
+            _playerStatsEvent.PlayerDamage =  _plusPlayerDamage;
+                                        
+            EventController.Instance.TriggerEvent(_playerStatsEvent);
+        }
+        
+    }
+}
