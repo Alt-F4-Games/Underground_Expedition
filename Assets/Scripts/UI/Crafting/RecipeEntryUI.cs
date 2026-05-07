132ffa0b67048343e3e8a68ad94511a028743a16
Emmanuel Zabala <emmanuelzabalamagrini@upc.edu.ar>
2026-05-07 18:50:45 -0300

    feat(crafting-ui): add recipe entry UI component
    - Added `RecipeEntryUI` component for crafting recipe entries
    - Added recipe icon support
    - Added recipe name display
    - Added recipe selection button handling
    - Connected recipe entries with `CraftingUIController`
    - Added dynamic setup logic for crafting recipes
    - Added click event binding for recipe selection

---
+++ Assets/Scripts/UI/Crafting/RecipeEntryUI.cs
@@ -0,0 +1,39 @@
+﻿using Network.Crafting;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI.Crafting
+{
+    public class RecipeEntryUI : MonoBehaviour
+    {
+        [Header("UI")]
+        [SerializeField] private Image icon;
+        [SerializeField] private TMP_Text recipeName;
+        [SerializeField] private Button button;
+
+        private CraftingRecipeSO _recipe;
+        private CraftingUIController _controller;
+
+        public void Setup(
+            CraftingRecipeSO recipe,
+            CraftingUIController controller)
+        {
+            _recipe = recipe;
+            _controller = controller;
+
+            var item = ItemDatabase.Instance.GetItemById(recipe.resultItemId);
+
+            icon.sprite = item.icon;
+            recipeName.text = item.itemName;
+
+            button.onClick.RemoveAllListeners();
+            button.onClick.AddListener(OnClicked);
+        }
+
+        private void OnClicked()
+        {
+            _controller.SelectRecipe(_recipe);
+        }
+    }
+}
