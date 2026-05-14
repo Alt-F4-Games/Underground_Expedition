a2a180c0fe994f3ac30e490fbbae44f07a9b4eee
Emmanuel Zabala <emmanuelzabalamagrini@upc.edu.ar>
2026-05-14 11:48:19 -0300

    feat(quests): add objective factory for runtime objective creation
    - Added ObjectiveFactory for dynamic objective runtime creation
    - Implemented factory pattern for quest objectives
    - Added support for:
      - Kill objectives
      - Collect objectives
      - Craft objectives
      - Interaction objectives
      - Exploration objectives
    - Decoupled quest runtime from concrete objective implementations
    - Prepared scalable objective instantiation pipeline
    - Enabled easy future extension for custom objective types

---
+++ Assets/Scripts/Network/Quests/Objectives/Core/ObjectiveFactory.cs
@@ -0,0 +1,51 @@
+﻿using Network.Quests.Definitions;
+using Network.Quests.Enums;
+using Network.Quests.Runtime;
+using Quests.Objectives;
+
+namespace Network.Quests.Objectives.Core
+{
+    public static class ObjectiveFactory
+    {
+        public static ObjectiveRuntimeBase CreateObjective(
+            QuestRuntime quest,
+            QuestObjectiveDefinition definition,
+            int stepIndex)
+        {
+            return definition.objectiveType switch
+            {
+                ObjectiveType.Kill =>
+                    new KillObjectiveRuntime(
+                        quest,
+                        definition,
+                        stepIndex),
+
+                ObjectiveType.Collect =>
+                    new CollectObjectiveRuntime(
+                        quest,
+                        definition,
+                        stepIndex),
+
+                ObjectiveType.Craft =>
+                    new CraftObjectiveRuntime(
+                        quest,
+                        definition,
+                        stepIndex),
+
+                ObjectiveType.Interact =>
+                    new InteractObjectiveRuntime(
+                        quest,
+                        definition,
+                        stepIndex),
+
+                ObjectiveType.Explore =>
+                    new ExploreObjectiveRuntime(
+                        quest,
+                        definition,
+                        stepIndex),
+
+                _ => null
+            };
+        }
+    }
+}
