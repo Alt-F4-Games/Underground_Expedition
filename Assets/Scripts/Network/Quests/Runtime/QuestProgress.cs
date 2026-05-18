78c7642d8bc237e94974d4543f7717127c5b2e53
Emmanuel Zabala <emmanuelzabalamagrini@upc.edu.ar>
2026-05-18 16:50:38 -0300

    chore(quests): change class locations to same script

--- Assets/Scripts/Network/Quests/Runtime/QuestProgress.cs
+++ Assets/Scripts/Network/Quests/Runtime/QuestProgress.cs
@@ -2,6 +2,16 @@
 
 namespace Network.Quests.Runtime
 {
+    public class QuestObjectiveProgress
+    {
+        public int CurrentAmount;
+
+        public int RequiredAmount;
+
+        public bool IsCompleted =>
+            CurrentAmount >= RequiredAmount;
+    }
+    
     public class QuestProgressData
     {
         public Dictionary<int, QuestObjectiveProgress>
