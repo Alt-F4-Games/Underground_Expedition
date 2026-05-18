398bf12af3e79cf532613c70584ffafd3b7bc5a5
Emmanuel Zabala <emmanuelzabalamagrini@upc.edu.ar>
2026-05-18 11:35:44 -0300

    feat(quests): add full quest objective type support for layer 2 progression

---
+++ Assets/Scripts/Network/Quests/Objectives/QuestObjectiveType.cs
@@ -0,0 +1,11 @@
+﻿namespace Network.Quests.Objectives
+{
+    public enum QuestObjectiveType
+    {
+        KillEnemy,
+        CollectItem,
+        CraftItem,
+        ExploreArea,
+        Interact
+    }
+}
