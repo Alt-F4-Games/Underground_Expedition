90e6a9642325e666f0d7bad802cb46d6e3be0efa
Emmanuel Zabala <emmanuelzabalamagrini@upc.edu.ar>
2026-05-27 20:08:09 -0300

    feat(runtime): add serialized quest progress data container

---
+++ Assets/Scripts/Network/Quests/Runtime/QuestProgressData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Network.Quests.Runtime
+{
+    [Serializable]
+    public struct QuestProgressData
+    {
+        public string QuestId;
+
+        public int StepIndex;
+
+        public int ObjectiveIndex;
+
+        public int CurrentAmount;
+    }
+}
