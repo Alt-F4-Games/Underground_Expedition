296a51a5ac7a9d3c86147835c94f7a944e6167eb
Emmanuel Zabala <emmanuelzabalamagrini@upc.edu.ar>
2026-06-11 18:29:14 -0300

    feat(audio): add AudioKeys constants for audio lookup

---
+++ Assets/Scripts/Audio/AudioKeys.cs
@@ -0,0 +1,33 @@
+﻿namespace Audio
+{
+    public static class AudioKeys
+    {
+      //================================================================
+      // RAT SOUNDS
+      //================================================================
+      
+      public const string RatDeath = "sfx_rat_death";
+      public const string RatAttack01 = "sfx_rat_attack01";
+      public const string RatAttack02 = "sfx_rat_attack02";
+      public const string RatWalk =  "sfx_rat_walk";
+      public const string RatDamaged01 = "sfx_rat_damaged01";
+      public const string RatDamaged02 = "sfx_rat_damaged02";
+      
+      //================================================================
+      // SKULL SOUNDS
+      //================================================================
+      
+      public const string SkullDeath = "sfx_skull_death";
+      public const string SkullDamaged =  "sfx_skull_damaged";
+      public const string SkullWalk =  "sfx_skull_walk";
+      public const string SkullExplotion =  "sfx_skull_explotion";
+      
+      //================================================================
+      // BOSS SOUNDS
+      //================================================================
+      
+      public const string BossEvoke =  "sfx_boss_evoke";
+      public const string BossSpawn =  "sfx_boss_spawn";
+      public const string BossWalk =  "sfx_boss_walk";
+    }
+}
